f60b7438d17a2fb4774b6d9322ff124643bc2ec4
Juan Esteban Diaz Salas <juan.diaz02@globant.com>
2024-02-23 18:32:39 -0300

    add mock

--- test/blocs/gps/gps_bloc_test.dart
+++ test/blocs/gps/gps_bloc_test.dart
@@ -4,9 +4,12 @@ import 'package:maps_app/blocs/blocs.dart';
 import 'package:maps_app/services/services.dart';
 import 'package:mockito/annotations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart';
-
-import '../../geolocator_test.dart';
+import '../../geolocator.dart';
+import '../../permission_handle.dart';
+import 'package:geolocator_platform_interface/src/enums/location_service.dart'
+    as geolocator_enum;
 
 @GenerateMocks([TrafficService])
 void main() {
@@ -14,26 +17,28 @@ void main() {
 
   setUp(() {
     WidgetsFlutterBinding.ensureInitialized();
-    gpsBloc = GpsBloc();
+    GeolocatorPlatform.instance = MockGeolocatorPlatform();
+    PermissionHandlerPlatform.instance = MockPermissionHandlerPlatform();
   });
 
-  tearDown(() async {
-    gpsBloc.close();
+  group('GpsAndPermissionEvent', () {
+    test('getServiceStatusStream', () {
+      when(GeolocatorPlatform.instance.getServiceStatusStream())
+          .thenAnswer((_) => Stream.value(geolocator_enum.ServiceStatus.enabled));
+
+      final locationService = Geolocator.getServiceStatusStream();
+
+      expect(locationService,
+          emitsInOrder([emits(geolocator_enum.ServiceStatus.enabled), emitsDone]));
+      gpsBloc = GpsBloc();
+    });
+
+    test('askPermissionGPS', () {
+      gpsBloc.askGpsAccess();
+    });
   });
-  test('Stream listen test', () {
-    gpsBloc.askGpsAccess();
-    var stream = gpsBloc.gpsServiceSubscription;
-    stream?.onData(
-      expectAsync1(
-        (event) {
-          expect(
-              event,
-              const GpsAndPermissionEvent(
-                isGpsEnabled: true,
-                isGpsPermissionGranted: true,
-              ));
-        },
-      ),
-    );
+
+  tearDownAll(() async {
+    gpsBloc.close();
   });
 }
